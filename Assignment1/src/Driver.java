02f75057187021e148b2a8b8fe0dc11b3fc05a1d
Adam Westerholm <adamwesterholm@sys-1.local>
2018-03-28 14:46:59 -0600

    in work changes

--- Assignment1/src/Driver.java
+++ Assignment1/src/Driver.java
@@ -2,18 +2,20 @@ import java.util.ArrayList;
 
 public class Driver
 {
+
     public static void main(String[] args) {
-        int[][] STP = new int[3][5];
+        int[][] STP = new int[5][3];
 
         int counter = 0;
-        for(int i = 0; i < 3; i ++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
                 STP[i][j] = counter;
-                counter ++;
+                counter++;
             }
-        }
 
-        ArrayList<int[][]> states_arrays = new ArrayList<>();
-        states_arrays.add(STP);
+
+            ArrayList<int[][]> states_arrays = new ArrayList<>();
+            states_arrays.add(STP);
+        }
     }
 }
