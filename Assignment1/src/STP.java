58a54907a166263ae942b7dafb1225bff9b19cd3
weste27 <adam.westerholm@du.edu>
2018-03-28 15:31:18 -0600

    adding changes

---
+++ Assignment1/src/STP.java
@@ -0,0 +1,53 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class STP {
+
+    public ArrayList<STPState> GetSuccessors (STPState stp)
+    {
+        ArrayList<STPState> retval = new ArrayList<>();
+
+        return retval;
+    }
+
+
+    //apply operator
+    public STPState ApplyOperator(STPState stp, SlideDir o)
+    {
+        switch (o){
+            case Up:
+
+            case Down:
+
+            case Left:
+
+            case Right:
+        }
+
+        return stp;
+    }
+
+    //undo operator
+    public STPState UndoOperator(STPState stp, SlideDir o)
+    {
+        switch(o){
+            case Up:
+
+            case Down:
+
+            case Left:
+
+            case Right:
+
+        }
+
+        return stp;
+    }
+
+
+    public enum SlideDir
+    {
+        Up, Down, Left, Right
+    }
+}
+
--- Assignment1/src/STPState.java
+++ Assignment1/src/STPState.java
@@ -2,12 +2,15 @@
 public class STPState
 {
     private int[][] arr;
+    private int xBlank;
+    private int yBlank;
     private String string = new String();
 
     public STPState()
     {
         arr = new int[5][3];
-
+        xBlank = 0;
+        yBlank = 0;
         int counter = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 3; j++) {
