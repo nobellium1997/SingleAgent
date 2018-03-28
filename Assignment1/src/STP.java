99418b14bc8cfea33d086c3713720a0611d1706f
weste27 <adam.westerholm@du.edu>
2018-03-28 16:51:17 -0600

    added changes

--- Assignment1/src/STP.java
+++ Assignment1/src/STP.java
@@ -2,11 +2,20 @@ import java.util.ArrayList;
 
 public class STP {
 
+    // How are we gonna call these functions in driver?
+
     public ArrayList<STPState> GetSuccessors (STPState stp)
     {
-        ArrayList<STPState> retval = new ArrayList<>();
+        ArrayList<STPState> succs = new ArrayList<>();
 
-        return retval;
+        return succs;
+    }
+
+    public ArrayList<SlideDir> GetOperators (STPState stp)
+    {
+        ArrayList<SlideDir> ops = new ArrayList<>();
+
+        return ops;
     }
 
 
@@ -22,38 +31,69 @@ public class STP {
         int x = stp.getxBlank();
         int y = stp.getyBlank();
 
-        //i is y
-        //j is x
+        //i is y (number of rows horizontal rows)
+        //j is x (number of vertical columns)
+        //remember that (0,0) is in top left corner
         switch (o){
             case Up:
-                //i--
+                //shifting the tile above the blank down to the current blank position
+                state[x][y] = state[x][y--];
 
+                //making the new blank position above the previous one
+                state[x][y--] = 0;
 
             case Down:
-                //i++
+                //shifting the tile below the blank up to the current blank position
+                state[x][y] = state[x][y++];
+
+                //making the new blank position below the previous one
+                state[x][y++] = 0;
 
             case Left:
-                //j--
+                //shifting the tile left of the blank over to the current blank position
+                state[x][y] = state[x--][y];
+
+                //making the new blank position to the left the previous one
+                state[x--][y] = 0;
 
             case Right:
-                //j++
+                //shifting the tile right of the blank over to the current blank position
+                state[x][y] = state[x++][y];
+
+                //making the new blank position to the left the previous one
+                state[x++][y] = 0;
 
         }
+        //TODO make int[][] constructor
+        //STPState retState = new STPState(state);
 
         return stp;
     }
 
-    //undo operator
+    //undo operator (same, but reversed)
     public STPState UndoOperator(STPState stp, SlideDir o)
     {
+        int[][] state = stp.getArr();
+        int x = stp.getxBlank();
+        int y = stp.getyBlank();
+
         switch(o){
             case Up:
+                state[x][y] = state[x][y++];
+                state[x][y++] = 0;
 
             case Down:
+                state[x][y] = state[x][y--];
+                state[x][y--] = 0;
 
             case Left:
+                state[x][y] = state[x++][y];
+                state[x++][y] = 0;
 
             case Right:
+                state[x][y] = state[x--][y];
+                state[x--][y] = 0;
+
 
         }
 
