bf3f648d29da1e9bd65e94f1c3b4478c0c1e41cf
nobbarak <nobel.barakat@du.edu>
2018-04-04 16:38:38 -0600

    fixed bug in bfs

--- Assignment1/src/BFS.java
+++ Assignment1/src/BFS.java
@@ -7,7 +7,16 @@ public class BFS {
     public static boolean breadthFirst(STPState start, STPState goal) {
         LinkedBlockingQueue<STPState> queue = new LinkedBlockingQueue<>();
         queue.add(start);
-        STPState parent = new STPState();
+        // this line is just for our janky implementation
+        // need to initialize parent to a state that's impossible to reach from the
+        // first iteration
+        int[][] arr = new int[5][3];
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 3; j++) {
+                arr[i][j] = 1;
+            }
+        }
+        STPState parent = new STPState(arr);
         while(!queue.isEmpty()) {
             STPState examineState = queue.remove();
             System.out.println(examineState);
