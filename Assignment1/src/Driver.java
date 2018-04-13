1807fd12606b54a960bbee7302db36639aa040a9
weste27 <adam.westerholm@du.edu>
2018-04-13 15:50:25 -0600

    changes

--- Assignment1/src/Driver.java
+++ Assignment1/src/Driver.java
@@ -24,8 +24,7 @@ public class Driver {
         stop = System.currentTimeMillis();
         System.out.println("Breadth first time " + (stop-start) + " milliseconds");
         System.out.println("asiodfiowejf");
-        System.out.println("asidojfiwejfwe");
-        System.out.println("asdfasdfasdfasdf");
+        System.out.println("please help");
     }
 
 
