039dad5a0474c94288fbce0418434b0ea6ef104c
nobbarak <nobel.barakat@du.edu>
2018-04-15 11:40:34 -0600

    changes

--- Assignment1/src/Driver.java
+++ Assignment1/src/Driver.java
@@ -19,21 +19,21 @@ public class Driver {
 //        System.out.println("Depth first time " + (stop-start) + " milliseconds");
 //        System.out.println();
 
-//        STPState s1 = new STPState();
-//        int[][] arr = new int[5][3];
-//        int counter = 0;
-//        for(int i = 0; i < 5; i ++) {
-//            for (int j = 0; j < 3; j++) {
-//                arr[i][j] = counter;
-//                counter++;
-//            }
-//        }
-//        arr[0][0] = 1;
-//        arr[0][1] = 0;
-//
-//        STPState s2 = new STPState(arr);
-//        Heuristic mh = new ManhattanDistance();
-//        System.out.println(mh.manhattan_distance(s1, s2));
+        STPState s1 = new STPState();
+        int[][] arr = new int[5][3];
+        int counter = 0;
+        for(int i = 0; i < 5; i ++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = counter;
+                counter++;
+            }
+        }
+        arr[0][0] = 1;
+        arr[0][1] = 0;
+
+        STPState s2 = new STPState(arr);
+        Heuristic mh = new ManhattanDistance();
+        System.out.println(mh.manhattan_distance(s1, s2));
 
     }
 
