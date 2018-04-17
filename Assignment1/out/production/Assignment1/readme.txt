9d45e6f61d167223580ef42bc57e0d72592d46d2
nobellium1997 <nobarakat@gmail.com>
2018-04-17 10:45:48 -0600

    changes

--- Assignment1/out/production/Assignment1/readme.txt
+++ Assignment1/out/production/Assignment1/readme.txt
@@ -1,17 +1,162 @@
-IDA* nodes expanded 269
-IDA* time 112 milliseconds
 
-DFID nodes expanded 12300543
-DFID time 14314 milliseconds
+Test1 nodes expanded 8
+Test1 time 11 milliseconds
 
-IDA* nodes expanded 283
-IDA* time 31 milliseconds
+Test2 nodes expanded 8
+Test2 time 0 milliseconds
 
-DFID nodes expanded 4457841
-DFID time 2535 milliseconds
+3  4  1
+6  8  2
+9  7  5
+10  13  11
+12  14  10
 
-IDA* nodes expanded 493
-IDA* time 60 milliseconds
+Test3 nodes expanded 8
+Test3 time 0 milliseconds
 
-DFID nodes expanded 39442736
-DFID time 20066 milliseconds
+Test4 nodes expanded 8
+Test4 time 0 milliseconds
+
+6  3  1
+4  0  8
+9  7  2
+10  13  5
+12  14  11
+
+Test5 nodes expanded 8
+Test5 time 0 milliseconds
+
+6  3  0
+9  8  1
+7  4  2
+10  13  5
+12  14  11
+
+Test6 nodes expanded 8
+Test6 time 0 milliseconds
+
+6  3  1
+9  0  8
+7  4  2
+10  13  5
+12  14  11
+
+Test7 nodes expanded 8
+Test7 time 0 milliseconds
+
+0  6  1
+9  3  8
+7  4  2
+10  13  5
+12  14  11
+
+Test8 nodes expanded 8
+Test8 time 0 milliseconds
+
+6  1  0
+9  3  8
+7  4  2
+10  13  5
+12  14  11
+
+Test9 nodes expanded 8
+Test9 time 0 milliseconds
+
+6  1  8
+9  2  4
+7  3  0
+10  13  5
+12  14  11
+
+Test10 nodes expanded 8
+Test10 time 0 milliseconds
+
+6  1  8
+9  2  4
+7  5  13
+10  14  3
+0  12  11
+
+Test11 nodes expanded 8
+Test11 time 0 milliseconds
+
+6  1  8
+9  0  2
+7  5  4
+14  12  13
+10  11  3
+
+Test12 nodes expanded 8
+Test12 time 0 milliseconds
+
+6  1  8
+9  5  2
+7  12  4
+14  13  13
+10  11  0
+
+Test13 nodes expanded 8
+Test13 time 0 milliseconds
+
+6  1  8
+9  5  2
+7  12  4
+14  0  13
+10  11  3
+
+Test14 nodes expanded 8
+Test14 time 0 milliseconds
+
+6  1  8
+9  5  2
+7  12  4
+14  0  11
+10  3  13
+
+Test15 nodes expanded 8
+Test15 time 1 milliseconds
+
+6  1  8
+9  5  2
+7  12  4
+14  0  11
+10  3  13
+
+Test16 nodes expanded 8
+Test16 time 0 milliseconds
+
+6  1  8
+9  5  2
+7  12  4
+10  14  11
+3  13  0
+
+Test17 nodes expanded 8
+Test17 time 0 milliseconds
+
+6  1  8
+9  5  2
+7  12  4
+10  0  14
+3  13  11
+
+Test18 nodes expanded 8
+Test18 time 0 milliseconds
+
+6  1  8
+9  5  2
+10  7  4
+12  0  14
+3  13  11
+
+Test19 nodes expanded 8
+Test19 time 1 milliseconds
+
+5  13  14
+2  4  12
+0  6  8
+3  1  9
+11  7  10
+
+Test20 nodes expanded 8
+Test20 time 0 milliseconds
--- Assignment1/src/IDAStar.java
+++ Assignment1/src/IDAStar.java
@@ -4,27 +4,43 @@ public class IDAStar {
     public static boolean is_found = false;
     public static int nodes_expanded = 0;
     public static STPState final_state;
+//    public static int fCost_limit;
+    public static ArrayList<Integer> fCost_collected;
+//    public static boolean is_assigned;
 
     public static ArrayList<SlideDir> GetPath(STPState start, STPState goal, Heuristic h) {
+        is_found = false;
+        nodes_expanded = 0;
+        final_state = null;
+        fCost_collected = new ArrayList<>();
         start.setParent(start);
         start.sethCost(h.manhattan_distance(start, goal));
         start.setgCost(0);
         start.setfCost(start.getgCost()+ start.gethCost());
-        int index = start.getfCost()+1;
+        fCost_collected.add(start.getfCost());
         while(!is_found) {
-            ida_star(start, goal, index, h);
-            index++;
+            int fCost_limit = fCost_collected.get(0);
+            fCost_collected.remove(0);
+            for(int i = 0; i < fCost_collected.size(); i++) {
+                if(fCost_limit > fCost_collected.get(i)) {
+                    fCost_limit = fCost_collected.get(i);
+                    fCost_collected.remove(i);
+                }
+            }
+            ida_star(start, goal, fCost_limit, h);
         }
         ArrayList<SlideDir> path = new ArrayList<>();
-        while(!final_state.equals(final_state.getParent())) {
-            path.add(final_state.getDirection());
-            final_state = new STPState(final_state.getParent());
+        STPState temp = new STPState(final_state);
+        while(!temp.equals(temp.getParent())) {
+            path.add(temp.getDirection());
+            temp = new STPState(temp.getParent());
         }
-        nodes_expanded = path.size();
+
         return path;
     }
 
     public static STPState ida_star(STPState start, STPState goal, int fCost, Heuristic h) {
+        nodes_expanded++;
         ArrayList<SlideDir> operators = STP.GetOperators(start);
         if(!start.getParent().equals(start)) {
             for(int i = 0; i < operators.size(); i++) {
@@ -40,12 +56,12 @@ public class IDAStar {
             }
             if(start.equals(goal)) {
                 System.out.println("STATE FOUND");
-//                System.out.print(start);
+                System.out.print(start);
                 is_found = true;
                 final_state = new STPState(start);
                 break;
             }
-            if(start.getfCost() < fCost) {
+            if(start.getfCost() <= fCost) {
                 SlideDir operator = operators.get(i);
                 STPState new_start = STP.ApplyOperator(start, operator);
                 new_start.setParent(start);
@@ -53,7 +69,11 @@ public class IDAStar {
                 new_start.setgCost(start.getgCost() + 1);
                 new_start.sethCost(h.manhattan_distance(new_start, goal));
                 new_start.setfCost(new_start.getgCost()+new_start.gethCost());
+//                System.out.println(new_start);
                 ida_star(new_start, goal, fCost, h);
+            } else {
+                fCost_collected.add(start.getgCost() + start.gethCost());
+                break;
             }
         }
 
